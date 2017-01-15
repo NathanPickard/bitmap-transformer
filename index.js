62c2bc4f72060c93f48beb7c47b7e4056cafd591
Nathan Pickard <nathan.pickard87@gmail.com>
2017-01-14 23:18:36 -0800

    read.js transforms file stream to inverted colors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const read = require('./lib/read.js');
 const stream = require('stream');
 
 function callBack() {
-  console.log('callback of readWrite');
+  console.log('callback of readWrite run');
 };
 
-read.readWrite('./non-palette-bitmap.bmp', callBack);
+read.readWrite('./LAND.bmp', callBack);
